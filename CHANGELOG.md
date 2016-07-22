4b8a423eec7046a1d84823be4e9187db1a4916d1
alevento <a@a.it>
2016-07-22 14:01:23 +0200

    Substitute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All Notable changes to `:package_name` will be documented in this file
 
-## alpha - @@@date
+## alpha - 22.07.16
 
 ### Added
 - Initial commit
--- tests/config/sedCommand.sh
+++ tests/config/sedCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/"psr-4": {/"psr-4": { "@@@namespacevendor\\\\@@@namespacepackage_name\\\\Test\\\\": ".\/vendor\/@@@vendor\/@@@package_name\/tests\/",/g' ./composer.json
+sed -i 's/"psr-4": {/"psr-4": { "Padosoft\\\\22072016packlara1\\\\Test\\\\": ".\/vendor\/padosoft\/22072016packlara1\/tests\/",/g' ./composer.json
